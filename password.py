61ac1d25eb710bb99f6286fab9e01ed845e7a4f1
Your Namecchsueh <md1004000@gmail.com>
2019-02-18 23:12:14 +0800

    new version

--- password.py
+++ password.py
@@ -1,14 +1,16 @@
 password = 'a123456'
 count = 3
 
-while count <= 3:
+while count > 0:
+    count = count - 1
     user_input = input('請輸入密碼(最多3次):')
     if user_input == password:
         print('登入成功')
         break
     else:
-        count = count - 1
-        print('密碼錯誤,剩下', count, '次機會!!')
-        if count == 0:
+        print('密碼錯誤!!')
+        if count > 0:
+            print('剩下', count, '次機會!!')
+        else:
             print('此帳號已被鎖定!!!')
             break
